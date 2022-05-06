a7626dfb47c877b61a1dab75f30791c6cb23d68a
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2022-05-05 23:43:21 -0400

    reading csv data directly from URL to json data

--- .github/repo_ci-cd/deployment/csv_to_json.py
+++ .github/repo_ci-cd/deployment/csv_to_json.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser
-from pathlib import Path
-from json import dumps as j_dumps
+# from argparse import ArgumentParser
+# from pathlib import Path
 import dask.dataframe as dd
 
-# https://docs.google.com/spreadsheets/d/14KVHigDGfs9yDVmw96mWSMAG2wRaFc7B9WtLuukm3Uk/gviz/tq?tqx=out:csv&sheet=Sheet1
 def main():
-    parser = ArgumentParser(description="""
-        This small python program convers csv files to json
-    """)
+    # parser = ArgumentParser(description="""
+    #     This small python program convers csv files to json
+    # """)
 
-    parser.add_argument('csv_input', type=Path, help="""
-        This is the path to the csv file you want to convert
-    """)
+    # parser.add_argument('csv_input', type=Path, help="""
+    #     This is the path to the csv file you want to convert
+    # """)
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
+
+    file_url = 'https://docs.google.com/spreadsheets/d/14KVHigDGfs9yDVmw96mWSMAG2wRaFc7B9WtLuukm3Uk/gviz/tq?tqx=out:csv&sheet=Sheet1'
 
     data_types = {
         'Video ID': 'object',
         'Photo ID': 'object',
     }
-    csv_df = dd.read_csv(args.csv_input,dtype = data_types).compute()
+    # csv_df = dd.read_csv(args.csv_input,dtype = data_types).compute()
+    csv_df = dd.read_csv(urlpath=file_url,dtype = data_types).compute()
     # https://stackoverflow.com/a/49551419
     # strips out leading and trailing whitespace for strings
     csv_df = csv_df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
